0f364d1ac61a89ed1061c57865b75c8cacc3068b
Pedro Henrique <pedrin000007@gmail.com>
2024-08-30 20:04:20 -0300

    Operadores aritiméticos com Java Script

---
+++ pages/aula05/part01/funcoes.js
@@ -0,0 +1,33 @@
+function adicao() {
+    var x = 2
+    var y = 2
+    let res = x + y
+    document.getElementById("resultado-adicao").innerHTML = res
+    document.getElementById("confirmado-adicao").innerHTML = "Resultado ✔"
+}
+function confirmado() {
+    document.getElementById("confirmado").innerHTML = "Resultado ✔"
+}
+function subtracao() {
+    var x = 2
+    var y = 2
+    let res = x - y
+    document.getElementById("resultado-subtracao").innerHTML = res
+    document.getElementById("confirmado-subtracao").innerHTML = "Resultado ✔"
+}
+
+function multiplicacao() {
+    var x = 2
+    var y = 2
+    let res = x * y
+    document.getElementById("resultado-multiplicacao").innerHTML = res
+    document.getElementById("confirmado-multiplicacao").innerHTML = "Resultado ✔"
+}
+function divisao() {
+    var x = 2
+    var y = 2
+    let res = x / y
+    document.getElementById("resultado-divisao").innerHTML = res
+    document.getElementById("confirmado-divisao").innerHTML = "Resultado ✔"
+}
+
